fc93b4b1a36bd9c93c3d228113d9e6501f9dffda
Ralf Schmitt <ralf@systemexit.de>
2011-06-20 16:39:34 +0200

    setup.py: add custom install command that disables the original _sqlite3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os, imp
 from distutils.core import setup, Extension
 sources = """cache.c cursor.c module.c row.c  util.c
 connection.c microprotocols.c prepare_protocol.c shell.c statement.c sqlite3.c""".split()
@@ -10,7 +10,33 @@ if sys.platform == "win32":
 else:
     define_macros = [("MODULE_NAME", '"sqlite3"')]
 
+from distutils.sysconfig import get_python_lib
+from distutils.command.install import install as _install
+
+
+class install(_install):
+    def run(self):
+        try:
+            path = imp.find_module("_sqlite3")[1]
+        except ImportError:
+            path = None
+
+        _install.run(self)
+
+        if path is None:
+            return
+
+        if path.startswith(get_python_lib()):
+            return
+
+        dirname, fn = os.path.split(path)
+        disabled = os.path.join(dirname, "disabled-" + fn)
+        print "mv %s %s" % (path, disabled)
+        os.rename(path, disabled)
+
+
 setup(name="_sqlite3",
       version="2.6.0-schmir1",
       description="make stdout unbuffered",
-      ext_modules=[Extension("_sqlite3", sources, define_macros=define_macros)])
+      ext_modules=[Extension("_sqlite3", sources, define_macros=define_macros)],
+      cmdclass=dict(install=install))
